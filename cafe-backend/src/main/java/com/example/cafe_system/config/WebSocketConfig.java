cdab240afdbd4ae8b555439aca04ee8f9fa7324e
vd558od <vladyslav.dorosh@student.tuke.sk>
2026-05-25 10:40:50 +0200

    add orders topic to WebSocketConfig

--- cafe-backend/src/main/java/com/example/cafe_system/config/WebSocketConfig.java
+++ cafe-backend/src/main/java/com/example/cafe_system/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic/kitchen", "/topic/bar");
+        registry.enableSimpleBroker("/topic/kitchen", "/topic/bar", "/topic/orders");
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
